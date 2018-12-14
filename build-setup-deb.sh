5c1261347ae5f4736c9274eeff2982194c98dc08
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-15 01:12:50 +0530

    Travis vuild fixed at last...

--- build-setup-deb.sh
+++ build-setup-deb.sh
@@ -7,53 +7,33 @@ sudo apt install build-essential gcc g++ make
 #install gtk dev & gnome-devel
 sudo apt install libgtk-3-dev gnome-devel
 
+#remove existing cmake
+sudo apt remove cmake
+sudo apt purge --auto-remove cmake
+
+#cmake version
+version=3.13
+build=2
+mkdir ~/tmp
+cd ~/tmp
+
+#get the cmake package
+wget https://github.com/Kitware/CMake/releases/download/v${version}.${build}/cmake-${version}.${build}.tar.gz
+tar -xzf cmake-${version}.${build}.tar.gz
+
+#install cmake
+cd cmake-${version}.${build}/
+./bootstrap
+make -j4
+sudo make install
+
 if [[ -z ${TRAVIS} ]];
 then
-     #remove existing cmake
-    sudo apt remove cmake
-    sudo apt purge --auto-remove cmake
-
-    #cmake version
-    version=3.13
-    build=2
-    mkdir ~/tmp
-    cd ~/tmp
-
-    #get the cmake package
-    wget https://github.com/Kitware/CMake/releases/download/v${version}.${build}/cmake-${version}.${build}.tar.gz
-    tar -xzf cmake-${version}.${build}.tar.gz
-
-    #install cmake
-    cd cmake-${version}.${build}/
-    ./bootstrap
-    make -j4
-    sudo make install
-
     #verify installations
     gcc -v
     g++ -v
     cmake --version
 else
-   echo "TRAVIS CI is running the build.\n No need to install latest cmake\n";
-   sudo apt remove cmake
-   sudo apt purge --auto-remove cmake
-
-    #cmake version
-    version=3.13
-    build=2
-    mkdir ~/tmp
-    cd ~/tmp
-
-    #get the cmake package
-    wget https://github.com/Kitware/CMake/releases/download/v${version}.${build}/cmake-${version}.${build}.tar.gz
-    tar -xzf cmake-${version}.${build}.tar.gz
-
-    #install cmake
-    cd cmake-${version}.${build}/
-    ./bootstrap
-    make -j4
-    sudo make install
-
     #verify installations
     gcc -v
     g++ -v
